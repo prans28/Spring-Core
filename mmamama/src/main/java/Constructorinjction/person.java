3dd693bf2d037cc4b625f9f51965cf921ad54ebf
prans <prans@pranav_kumar>
2023-04-28 04:11:52 +0530

    //

--- mmamama/src/main/java/Constructorinjction/person.java
+++ mmamama/src/main/java/Constructorinjction/person.java
@@ -1,21 +1,25 @@
 package Constructorinjction;
 
+import java.util.List;
+
 public class person {
 	
   private String name ; 
   private int age ; 
   private certi c;
+  private List<String> list;
   
-  public person(String name , int age ,certi c) {
+  public person(String name , int age ,certi c ,List<String> list) {
 	  this.age = age ; 
 	  this.name  = name; 
 	  this.c = c;
+	  this.list=list;
   }
 
   
   
   public String toString() {
-	  return this.name + ":"+ this.age + ":"+ "{"+this.c.name+"}";
+	  return this.name + ":"+ this.age + ":"+ "{"+this.c.name+"}"+list;
   }
 
 }
